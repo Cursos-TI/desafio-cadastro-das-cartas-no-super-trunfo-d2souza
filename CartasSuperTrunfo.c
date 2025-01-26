a2fd1453d78ad712beb9eef6d99c5595511b61a7
Daniel Souza <68620321+d2souza@users.noreply.github.com>
2025-01-26 15:35:19 +0000

    DESAFIO COMPLETO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,56 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+   
+    //DECLARAÇÃO DE VARIAVEIS
+    int populacao;
+    float area;
+    float pib;
+    int numeroDePontosTuristicos;
+    char estado;
+    char nomeDaCidade[30];
+    int codigoDaCarta;
+
+    // ENTRADAS DO USUÁRIO
+
+    printf("Digite o estado (de 'A' a 'H'): ");
+    scanf("%c", &estado);
+
+    printf("Digite a cidade(de 1 a 4): ");
+    scanf("%d", &codigoDaCarta);
+    getchar();
+
+    printf("Digite o numero de Pontos Turisticos: ");
+    scanf("%d", &numeroDePontosTuristicos);
+
+    printf("Digite a area total da cidade em quilometros quadrados: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &pib);
+    getchar();
+
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao);
+    getchar();
+
+    //TIVE QUE USAR UM TRATAMENTO DIFERENTE NA ENTRADA DO USUÁRIO PARA RECEBER A STRING COM CARACTERE DE ESPAÇO
+    printf("Digite o nome da cidade: ");
+    fgets(nomeDaCidade, sizeof(nomeDaCidade), stdin);
+    //VERIFICA SE HÁ QUEBRA DE LINHA E SUBSTITUI PARA ENCERRAR A ENTRADA
+    nomeDaCidade[strcspn(nomeDaCidade, "\n")] = '\0';
+
+    //IMPRIME O RESULTADO DE FORMA ORDENADA
+
+    printf("***********************\n");
+    printf("Estado: %c\n", estado);
+    printf("Codigo da Carta: %c0%d\n", estado ,codigoDaCarta);
+    printf("Nome da Cidade: %s\n", nomeDaCidade);
+    printf("Populacao: %d\n", populacao);
+    printf("Area: %.2f km2\n", area);
+    printf("PIB: %.2f bilhoes de reais\n", pib);
+    printf("Numero de pontos turisticos: %d\n", numeroDePontosTuristicos);
+    printf("***********************");
 
     return 0;
 }
